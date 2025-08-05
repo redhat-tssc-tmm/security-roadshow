f4f0b70871ccbfc800c6c326d8512beef5810261
Markus J. Nagel <mnagel@redhat.com>
2025-08-05 19:40:38 +0200

    Switch to final

--- cleanup_tssc-module.sh
+++ cleanup_tssc-module.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-# Check if both parameters are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <quayuser> <quaypassword>"
-    exit 1
-fi
 
 # Assign parameters to variables
 QUAYUSER="$1"
 QUAYPASSWORD="$2"
 
+cd ~/demo-apps
 
-# ASCII art warning sign
-echo "   ╔═════════════════════╗"
-echo "   ║                     ║"
-echo "   ║   ⚠️  CAUTION  ⚠️     ║"
-echo "   ║                     ║"
-echo "   ║  CONSTRUCTION SITE  ║"
-echo "   ║                     ║"
-echo "   ║                     ║"
-echo "   ║   MIND YOUR STEP!   ║"
-echo "   ║                     ║"
-echo "   ╚═════════════════════╝"
-echo "            ║   ║"
-echo "            ║   ║"
-echo "        ════╩═══╩════"
+echo "====================================================="
+echo "TSSC MODULE CLEANUP SCRIPT"
+echo "====================================================="
 echo ""
-
-# Print the parameters
-echo "Parameters received:"
-echo "Quay User: $QUAYUSER"
-echo "Quay Password: $QUAYPASSWORD"
+echo "STEP 1 - remove git repo from gitlab"
 echo ""
+curl -fsSL https://raw.githubusercontent.com/redhat-tssc-tmm/security-roadshow/main/gitlab_cleanup_script.sh | bash -s -- --execute
+echo ""
+echo "STEP 2 - remove local git repo"
+cd 
+rm -rf ~/demo-apps
+echo ""
+echo "STEP 3 - remove signatures from quay repository"
+echo ""
+curl -fsSL https://raw.githubusercontent.com/redhat-tssc-tmm/security-roadshow/main/podman_signature_removal.sh | bash -s $1 $2
+echo ""
+echo "====================================================="
+echo "END TSSC MODULE CLEANUP SCRIPT"
+echo "====================================================="
+echo ""
+echo ""
+echo "=== To cleanup sigstore environment variables, run this command (optional) ==="
+echo "eval \"\$(curl -s https://raw.githubusercontent.com/redhat-tssc-tmm/security-roadshow/main/unset_sigstore_env_vars.sh)\""
+echo "=================================================="
+echo ""
+echo ""
+curl -s https://raw.githubusercontent.com/redhat-tssc-tmm/security-roadshow/main/banner.txt
